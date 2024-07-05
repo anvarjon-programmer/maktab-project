bbb3b912a042aa6320be307ab86cd47e6cc14977
anvarjon-programmer <mranvarjontojiboyev@gmail.com>
2024-07-05 14:14:57 +0300

    fix error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,13 @@ const Header = () => {
            <Link to={'/'}>
             <img src={logo} alt="" className='w-[110px]'/>
            </Link>
-           <h1 className='max-w-[800px] text-[22px] text-[#2e3192] text-center  font-semibold'>O‘ZBEKISTON RESPUBLIKASI
+           <h1 className='max-w-[800px] text-[22px] text-[#2e3192] text-center  font-semibold'>
+                <div>
+                O‘ZBEKISTON RESPUBLIKASI
                 MADANIYAT VAZIRLIGI
-                QASHQADARYO VILOYATI
+                QASHQADARYO 
+                </div>
+                VILOYATI
                 MADANIYAT BOSHQARMASI
                 MIRISHKOR TUMANI
                 19-SON BOLALAR MUSIQA VA SAN'AT MAKTABI</h1>
