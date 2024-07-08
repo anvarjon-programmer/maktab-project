852002d276b299d2436e855013e3ec9cbc65a77a
Anvarjon <143145991+anvarjon-programmer@users.noreply.github.com>
2024-07-08 13:16:30 +0500

    fixed carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,63 @@
-import React from 'react'
+import React from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+// import './carousel.css'; // Import the custom CSS file
 import Slider from "react-slick";
+import swagger from '../services/swagger-img';
 
 const Home = () => {
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      }
+    ]
   };
 
   return (
-    <div className='h-screen container py-4'>
-      <Slider {...settings}>
+    <div className='h-screen w-full container max-w-[1100px] pt-5 ttttt'>
       <div>
-        <img src="https://img.freepik.com/free-photo/texture-treble-clef-dark-background-isolated-generative-ai_169016-29581.jpg" alt="" />
+      <Slider {...settings} className='max-w-[1100px] m-auto '>
+        {
+          swagger.map((item, index) => (
+            <div key={index} className='md:max-w-[300px] h-[200px] px-2'>
+              <img className='object-cover w-full h-full' src={item.img} alt="" />
+            </div>
+          ))
+        }
+      </Slider>
       </div>
-      <div>
-        <img src="https://media.istockphoto.com/id/1076840920/vector/music-background.jpg?s=612x612&w=0&k=20&c=bMG2SEUYaurIHAjtRbw7bmjLsXyT7iJUvAM5HjL3G3I=" alt="" />
-      </div>
-      <div>
-      <img src="https://img.freepik.com/free-photo/texture-treble-clef-dark-background-isolated-generative-ai_169016-29581.jpg" alt="" />
-      </div>
-      <div>
-      <img src="https://media.istockphoto.com/id/1076840920/vector/music-background.jpg?s=612x612&w=0&k=20&c=bMG2SEUYaurIHAjtRbw7bmjLsXyT7iJUvAM5HjL3G3I=" alt="" />
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/services/swagger-img.js
@@ -0,0 +1,47 @@
+
+import swagger1 from '../assets/swagger/swaggeer1.jpg'
+import swagger2 from '../assets/swagger/swaggeer2.jpg'
+import swagger3 from '../assets/swagger/swaggeer3.jpg'
+import swagger4 from '../assets/swagger/swaggeer4.jpg'
+import swagger5 from '../assets/swagger/swaggeer5.jpg'
+import swagger6 from '../assets/swagger/swaggeer6.jpg'
+import swagger7 from '../assets/swagger/swaggeer7.jpg'
+import swagger8 from '../assets/swagger/swaggeer8.jpg'
+import swagger9 from '../assets/swagger/swaggeer9.jpg'
+import swagger10 from '../assets/swagger/swaggeer10.jpg'
+
+
+const swagger = [
+    {
+        img:swagger1
+    },
+    {
+        img:swagger2
+    },
+    {
+        img:swagger3
+    },
+    {
+        img:swagger4
+    },
+    {
+        img:swagger5
+    },
+    {
+        img:swagger6
+    },
+    {
+        img:swagger7
+    },
+    {
+        img:swagger8
+    },
+    {
+        img:swagger9
+    },
+    {
+        img:swagger10
+    },
+]
+
+export default swagger
