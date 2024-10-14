0ffd1025ff8e78b78a1f23930618a619a198a065
Anvarjon <mranvarjontojiboyev@gmail.com>
2024-10-14 20:48:20 +0500

    fix raxbariyat

--- src/pages/BMS-togrisida/Maktab-rahbaryati.jsx
+++ src/pages/BMS-togrisida/Maktab-rahbaryati.jsx
@@ -4,6 +4,7 @@ import raxbar1 from '../../assets/RAXBAR1.jpg'
 import raxbar2 from '../../assets/ТЕМИРОВ-ШАРОФИДДИН.jpg'
 import raxbar3 from '../../assets/ЎРОЗОВ-ДЎСМУРОД.jpg'
 import raxbar4 from '../../assets/АГАНИЯЗОВ-ТОЛИБЖОН.jpg'
+import Ruzikulovich from '../../assets/Ruzikulovich.jpg'
 
 
 const MaktabRahbaryati = () => {
@@ -37,8 +38,8 @@ const MaktabRahbaryati = () => {
 
       <div className='bg-white max-w-[1000px] mt-10 m-auto '>
         <div className='p-10 md:flex items-center justify-between gap-8'>
-          <div className='md:max-w-[300px]'>
-           <img className='w-full' src={raxbar3} alt="" />
+          <div className='md:max-w-[400px]'>
+           <img className='w-[300px] h-[340px] object-cover' src={Ruzikulovich} alt="" />
           </div>
           <div className='mt-7 md:mb-10'>
             <h2 className='text-3xl font-bold'>O'razov Do'smurod Ruzikulovich</h2>
